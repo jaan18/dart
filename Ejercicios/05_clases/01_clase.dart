72794fe3a0bda02fa73c8f4bad118db14056a736
jaan18 <jovas28@hotmail.com>
2019-10-03 12:08:45 -0500

    clases en dart

--- Ejercicios/05_clases/01_clase.dart
+++ Ejercicios/05_clases/01_clase.dart
@@ -1,3 +1,11 @@
+import 'clases/persona.dart';
+
 main() {
-  
+  final persona = new Persona();
+
+  persona.nombre = "Jovani";
+  persona.edad = 27;
+  persona.bio = "Nacio en California";
+
+  print(persona);
 }
---
+++ Ejercicios/05_clases/clases/persona.dart
@@ -0,0 +1,13 @@
+class Persona {
+  // Campos o propiedades
+  String nombre;
+  int edad;
+  String bio;
+  // Gets y Sets
+
+  // Constructores
+
+  // métodos
+  @override
+  String toString() => '$nombre, $edad, $bio';
+}
